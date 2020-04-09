a221b00fe0d28921497a514e2b3fe33cb5fdb984
barney-heatwave <djdise@outlook.com>
2020-04-09 23:45:35 +0100

    Add autojump

--- cl_dll/hud.cpp
+++ cl_dll/hud.cpp
@@ -367,6 +367,7 @@ void CHud :: Init( void )
 	m_Flash.Init();
 	m_Message.Init();
 	m_StatusBar.Init();
+	m_Speedometer.Init();
 	m_DeathNotice.Init();
 	m_AmmoSecondary.Init();
 	m_TextMessage.Init();
@@ -515,6 +516,7 @@ void CHud :: VidInit( void )
 	m_Message.VidInit();
 	m_StatusBar.VidInit();
 	m_DeathNotice.VidInit();
+	m_Speedometer.VidInit();
 	m_SayText.VidInit();
 	m_Menu.VidInit();
 	m_AmmoSecondary.VidInit();
