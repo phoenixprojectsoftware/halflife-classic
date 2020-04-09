647c534b83b572956f91d74b248ade9332351e1f
barney-heatwave <djdise@outlook.com>
2020-04-09 22:34:41 +0100

    Fix mp5 fire rate change

--- dlls/mp5.cpp
+++ dlls/mp5.cpp
@@ -129,7 +129,7 @@ BOOL CMP5::Deploy( )
 void CMP5::PrimaryAttack()
 {
 #ifndef CLIENT_DLL
-	int MP5OldRate = mp5_old_rate.value;
+	int mp5OldRate = mp5_old_rate.value;
 #endif
 	// don't fire underwater
 	if (m_pPlayer->pev->waterlevel == 3)
@@ -191,8 +191,10 @@ void CMP5::PrimaryAttack()
 
 	m_flNextPrimaryAttack = GetNextAttackDelay(0.1);
 	if ( m_flNextPrimaryAttack < UTIL_WeaponTimeBase() )
+#ifndef CLIENT_DLL
 		if (mp5_old_rate.value==0) m_flNextPrimaryAttack = UTIL_WeaponTimeBase() + 0.1;
-	if (mp5_old_rate.value == 1) m_flNextPrimaryAttack = UTIL_WeaponTimeBase() + 0.09;
+	if (mp5_old_rate.value == 1) m_flNextPrimaryAttack = UTIL_WeaponTimeBase() + 0.08;
+#endif
 
 	m_flTimeWeaponIdle = UTIL_WeaponTimeBase() + UTIL_SharedRandomFloat( m_pPlayer->random_seed, 10, 15 );
 }
