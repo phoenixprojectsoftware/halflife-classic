d4eec54087dd15e673ff68195cc8bfcfe14c7acc
barney-heatwave <djdise@outlook.com>
2020-04-11 16:13:14 +0100

    Revert "Add autojump"
    This reverts commit 274b7f5fe5e0068b4691443fccefb09000e81b67.

--- cl_dll/input.cpp
+++ cl_dll/input.cpp
@@ -64,7 +64,6 @@ cvar_t	*cl_yawspeed;
 cvar_t	*cl_pitchspeed;
 cvar_t	*cl_anglespeedkey;
 cvar_t	*cl_vsmoothing;
-cvar_t* cl_autojump;
 /*
 ===============================================================================
 
@@ -992,8 +991,6 @@ void InitInput (void)
 	cl_movespeedkey		= gEngfuncs.pfnRegisterVariable ( "cl_movespeedkey", "0.3", 0 );
 	cl_pitchup			= gEngfuncs.pfnRegisterVariable ( "cl_pitchup", "89", 0 );
 	cl_pitchdown		= gEngfuncs.pfnRegisterVariable ( "cl_pitchdown", "89", 0 );
-	cl_autojump = gEngfuncs.pfnRegisterVariable("cl_autojump", "1", FCVAR_ARCHIVE);
-
 
 	cl_vsmoothing		= gEngfuncs.pfnRegisterVariable ( "cl_vsmoothing", "0.05", FCVAR_ARCHIVE );
 
--- pm_shared/pm_shared.cpp
+++ pm_shared/pm_shared.cpp
@@ -28,7 +28,6 @@
 #include <string.h> // strcpy
 #include <stdlib.h> // atoi
 #include <ctype.h>  // isspace
-#include <cvardef.h>
 
 #ifdef CLIENT_DLL
 	// Spectator Mode
@@ -2464,14 +2463,13 @@ void PM_NoClip()
 	fraction = ( maxscaledspeed / spd ) * 0.65; //Returns the modifier for the velocity
 	
 	VectorScale( pmove->velocity, fraction, pmove->velocity ); //Crop it down!.
-}
-*/
+}*/
+
 /*
 =============
 PM_Jump
 =============
 */
-extern cvar_t* cl_autojump;
 void PM_Jump (void)
 {
 	int i;
@@ -2552,8 +2550,8 @@ void PM_Jump (void)
 		return;		// in air, so no effect
 	}
 
-	if (cl_autojump->value==1) if ( pmove->oldbuttons & IN_JUMP )
-		return;		// don't pogo stick
+	/*if ( pmove->oldbuttons & IN_JUMP )
+		return;		// don't pogo stick*/
 
 	// In the air now.
     pmove->onground = -1;
